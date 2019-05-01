c1cc7417804e55b8b16c7a7375cbd029363593b6
rikxjeee <gyuszi02@gmail.com>
2019-05-01 11:23:28 +0200

    Update code style

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+require 'vendor/autoload.php';
+
+if ($argc > 1) {
+    $items = explode(",", $argv[1]);
+} else {
+    echo("Wrong arguments!");
+    exit (1);
+}
+
+
+$a = new Load\classes\SortItems();
+$b = new Load\classes\Cart();
+
+echo $b->calculatePrice($a->sort($items)) . "\n";
