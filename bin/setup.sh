153efe5e2d5b0cfd1e1a48d1434bccfe9b99dfa5
tkotosz <tkotosz@inviqa.com>
2019-08-01 18:04:11 +0200

    docker fixes

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cd ../docker || exit
-docker-compose up -d
+DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
+
+cd $DIR/../docker && docker-compose stop && docker-compose up -d && cd - || exit
 while ! docker exec smarket-mysql mysqladmin --user=root --password=simple --host "127.0.0.1" ping --silent &> /dev/null ; do
     echo "Waiting for database connection..."
     sleep 5
 done
-cd ../bin || exit
 echo 'Populating database with development data.'
-docker exec smarket-php php /site/bin/setup.php
+cd $DIR/../bin && docker exec smarket-php php /site/bin/setup.php && cd -|| exit
 exitcode=$?
 
 if [ $exitcode == 0 ]; then
-  composer install -a
+  cd $DIR/../docker && docker-compose exec php-fpm composer install -a && cd -
     echo 'Operation succesful.'
     else
       echo 'Operation failed, try again.'
